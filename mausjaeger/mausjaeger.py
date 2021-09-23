e3e835222c5f15f1123c799397cebdd234605326
de-wax <45847235+de-wax@users.noreply.github.com>
2021-09-23 15:08:14 +0200

    Changed logging settings

--- mausjaeger/mausjaeger.py
+++ mausjaeger/mausjaeger.py
@@ -29,9 +29,8 @@ rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 print(str(datetime.datetime.now()) + " [INFO] warming up...")
 time.sleep(conf["camera_warmup_time"])
 avg = None
-# only for debugging
-#c_count = 0
-#c_avg = 0
+c_count = 0
+c_avg = 0
 
 # capture frames from the camera
 for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -69,15 +68,13 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     # loop over the contours
     for c in cnts:
         contours = cv2.contourArea(c)
-        # only for debugging
-        #if c_count >= conf["c_avg_count"]:
-        #    print(str(timestamp) + " [INFO] Average contours on last " + str(conf["c_avg_count"]) + " images: " + str(c_avg / conf["c_avg_count"]))
-        #    c_count = 0
-        #    c_avg = 0
-        #else:
-        #    c_count += 1
-        #    c_avg += int(contours)
-        #print(str(timestamp) + " [DEBUG] c_count: " + str(c_count))
+        if c_count >= conf["c_avg_count"]:
+            print(str(timestamp) + " [INFO] Average contours on last " + str(conf["c_avg_count"]) + " images: " + str(c_avg / conf["c_avg_count"]))
+            c_count = 0
+            c_avg = 0
+        else:
+            c_count += 1
+            c_avg += int(contours)
         # detect movement
         if contours >= conf["min_contours"]:
             print(str(timestamp) + " [INFO] Saving image...")
